95191a411ed643ff748e4441b098617b3fb3635f
Azimbek <azimbekxaydarov946@gmail.com>
2022-08-13 12:06:41 +0500

    Initial commit

---
+++ app/Http/Livewire/NewsComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class NewsComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.news-component');
+    }
+}
