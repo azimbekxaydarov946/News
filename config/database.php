d7116e5df0a67859b13581b51dce759c6409cf6d
Azimbek <azimbekxaydarov946@gmail.com>
2022-09-02 12:29:32 +0500

    Revert "start project heroku"
    This reverts commit 47ab12333ce84b77e7557c05929a0bea495e54b8.

--- config/database.php
+++ config/database.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL=parse_url('DATABASE_URL');
-
 return [
 
     /*
@@ -17,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -48,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => $DATABASE_URL['host'],
-            'port' =>  $DATABASE_URL['port'],
-            'database' => ltrim($ $DATABASE_URL['path'], '/'),
-            'username' => $DATABASE_URL['user'],
-            'password' =>  $DATABASE_URL['password'],
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
