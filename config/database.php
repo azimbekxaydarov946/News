2c9a7964ff6e5e0cc8bf9fb51e73f089508cc95e
Azimbek <azimbekxaydarov946@gmail.com>
2022-09-03 07:19:30 +0500

    Revert "update database comfig"
    This reverts commit 01b2552837c28fe2dc3467794187fb912cf06eef.

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,7 +66,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'https://aznewsuz.herokuapp.com/'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
