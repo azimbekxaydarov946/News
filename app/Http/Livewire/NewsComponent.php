030328e298cdcaf79e2c8041da6a85195673c63e
Azimbek <azimbekxaydarov946@gmail.com>
2022-09-03 07:18:52 +0500

    Revert "factory edit"
    This reverts commit a7bd59499350bdad456b36e453d4810e06ae7409.

--- app/Http/Livewire/NewsComponent.php
+++ app/Http/Livewire/NewsComponent.php
@@ -7,7 +7,6 @@ use App\Models\Comment;
 use App\Models\News;
 use App\Models\Tag;
 use App\Models\User;
-use Illuminate\Support\Str;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -77,7 +76,7 @@ class NewsComponent extends Component
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-          ]);
+          ])
           }
       }
 
