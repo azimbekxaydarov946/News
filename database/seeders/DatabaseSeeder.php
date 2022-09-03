e46f2d3276266dc0221df7311ff5ed2d22e91dc0
Azimbek <azimbekxaydarov946@gmail.com>
2022-09-03 07:19:21 +0500

    Revert "factory edit"
    This reverts commit dbe8e859302c2898f9e6733ecbadd2e8130512d5.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
-        // $this->call(CommentSeeder::class);
-        // $this->call(NewsSeeder::class);
-        // $this->call(TagsSeeder::class);
+        $this->call(CommentSeeder::class);
+        $this->call(NewsSeeder::class);
+        $this->call(TagsSeeder::class);
     }
 }
