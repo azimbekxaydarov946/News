6030e4873e31eff553bb9dd126841a1dcd7e6bed
Azimbek <azimbekxaydarov946@gmail.com>
2022-09-03 07:18:42 +0500

    Revert "factory edit"
    This reverts commit 5ea3a84dcb6cb8be6ba347228ac0d5fe5e2247d8.

--- app/Http/Livewire/NewsComponent.php
+++ app/Http/Livewire/NewsComponent.php
@@ -73,7 +73,7 @@ class NewsComponent extends Component
                 ]);
                 User::create([
                     'name' => 'Jon',
-                    'email' => 'jon'.$i.'@gmail.com',
+                    'email' => 'jon@gmail.com',
                     'email_verified_at' => now(),
                     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                     'remember_token' => Str::random(10),
